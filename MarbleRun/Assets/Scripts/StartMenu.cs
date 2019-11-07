7296a7fd02ba98598aa5ded2ffe6503a341e398d
KHicok <kolehicok@gmail.com>
2019-11-07 12:09:43 -0600

    Creating initial functioning Start Menu

---
+++ MarbleRun/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartButtonPress()
+    {
+        SceneManager.LoadScene("Scene Selection");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
