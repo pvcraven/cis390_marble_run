c2b2cd9edc12d9aaa3b2287da402326829a5fcd9
karson_k09 <karson.richardson@my.simpson.edu>
2019-11-13 16:51:24 -0600

    Added script for better camera control

---
+++ MarbleRun/Assets/Scripts/CheckLeadBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLeadBall : MonoBehaviour
+{
+    public GameObject currentLeadBall;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (currentLeadBall == null && other.tag.Equals("Player"))
+        {
+            other.tag = "LeadBall";
+            currentLeadBall = other.gameObject;
+            Destroy(gameObject);
+        }
+        else if (currentLeadBall != null && other.tag.Equals("Player"))
+        {
+            other.tag = "LeadBall";
+            currentLeadBall.tag = "Player";
+            currentLeadBall = other.gameObject;
+            Destroy(gameObject);
+        }
+        else if (other.tag.Equals("LeadBall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
