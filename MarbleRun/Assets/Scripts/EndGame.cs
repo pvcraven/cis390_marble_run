2d370a04905de3505c78746293249d57c1ea9296
pmcburney <payton.mcburney@my.simpson.edu>
2019-12-05 13:11:27 -0600

    Added which marble won to end menus.

--- MarbleRun/Assets/Scripts/EndGame.cs
+++ MarbleRun/Assets/Scripts/EndGame.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
+    GameObject scoreboardCanvas;
 
     // Start is called before the first frame update
+    private void Start()
+    {
+        // Get the name of the winning marble
+        scoreboardCanvas = GameObject.Find("Scoreboard Canvas");
+        GameObject firstPlaceCell = scoreboardCanvas.transform.GetChild(0).transform.GetChild(3).gameObject;
+        string winningMarble = firstPlaceCell.GetComponent<Text>().text;
+
+        // Display the name of the winning marble
+        GameObject endGameMessage = this.transform.GetChild(1).gameObject;
+        endGameMessage.GetComponent<Text>().text = winningMarble + " won!";
+    }
+
     public void StartButtonPress()
     {
-        GameObject scoreboardCanvas = GameObject.Find("Scoreboard Canvas");
         Destroy(scoreboardCanvas);
         SceneManager.LoadScene("Scene Selection");
     }
